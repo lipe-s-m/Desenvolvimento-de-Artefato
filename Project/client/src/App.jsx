c08ad9ecabf997dacc943baf79c66fcfd53bc286
lipe-s-m <felipe.s.m1609@gmail.com>
2024-04-25 12:13:52 -0300

    version 3

---
+++ Project/client/src/App.jsx
@@ -0,0 +1,19 @@
+import Login from "./Pages/Login/LoginPage";
+import Home from "./Pages/Home/Home";
+
+function App() {
+  const usuarioLogadoString = localStorage.getItem("_usuario_logado");
+  const usuarioLogado = JSON.parse(usuarioLogadoString);
+  console.log(usuarioLogado);
+
+  //HTML ABAIXO
+  return (
+    <>
+      <h1>App diz: ola, {usuarioLogado.name}, bem vindo!</h1>
+      <Login />
+      <Home />
+    </>
+  );
+}
+
+export default App;
